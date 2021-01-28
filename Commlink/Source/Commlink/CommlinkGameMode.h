0370a66d84b8ec5b70beedf3ab37551d7a047166
Richard Van Tassel <rickvt@gmail.com>
2021-01-27 21:47:14 -0500

    move gitignore to proper location. Add missing files.

---
+++ Commlink/Source/Commlink/CommlinkGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CommlinkGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMMLINK_API ACommlinkGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
